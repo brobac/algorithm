55bac76fd27030e8411fb072825e93d8b3078b7d
brobac <issg2030@gmail.com>
2023-12-14 23:01:05 +0900

    23.12.14(목) 1문제

--- baekjoon/9095.js
+++ baekjoon/9095.js
@@ -2,4 +2,23 @@ const nums = require("fs")
   .readFileSync("test.txt")
   .toString()
   .trim()
-  .split("\n");
+  .split("\n")
+  .slice(1);
+
+const dp = Array(11);
+dp[1] = 1;
+dp[2] = 2;
+dp[3] = 4;
+dp[4] = 7;
+
+for (let i = 5; i < 11; i++) {
+  dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+}
+
+const result = [];
+
+nums.forEach((n) => {
+  result.push(dp[n]);
+});
+
+console.log(result.join("\n"));
