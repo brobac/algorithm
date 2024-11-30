675d2119ffe61cc3e8a2a5a2bf46d95f37b4a654
HyeongJun Park <69460842+brobac@users.noreply.github.com>
2024-11-30 18:35:24 +0900

    Title: 코드트리 투어, Time: 206ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241130/코드트리 투어/codetree-tour.java
+++ 241130/코드트리 투어/codetree-tour.java
@@ -5,63 +5,49 @@ import java.util.*;
 
 public class Main {
     static int Q, N, M, s;
-    static boolean calculated;
     static ArrayList<int[]>[] adjList;
     static PriorityQueue<Product> bestQ;
-    static Product[] products;
-    static ArrayList<Product> unCalculatedProductList;
+    static boolean[] canceled;
+    static int[] minCost;
 
 
-    static int getGain(Product p) {
-
-        boolean[] visited = new boolean[N];
-        int[] minCost = new int[N + 1];
+    static void calculate() {
+        minCost = new int[N];
         Arrays.fill(minCost, Integer.MAX_VALUE);
         minCost[s] = 0;
-
+        boolean[] visited = new boolean[N];
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
         pq.offer(new int[]{s, 0});
         while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int vertex = cur[0];
-            int cost = cur[1];
+            int city = pq.poll()[0];
 
-            if (visited[vertex]) continue;
-            visited[vertex] = true;
+            if (visited[city]) continue;
+            visited[city] = true;
 
-            for (int[] adj : adjList[vertex]) {
-                int adjVertex = adj[0];
+            for (int[] adj : adjList[city]) {
+                int adjCity = adj[0];
                 int adjCost = adj[1];
-                if (adjCost + minCost[vertex] < minCost[adjVertex]) {
-                    minCost[adjVertex] = adjCost + minCost[vertex];
-                    pq.offer(new int[]{adjVertex, minCost[adjVertex]});
+                if (minCost[city] + adjCost < minCost[adjCity]) {
+                    minCost[adjCity] = minCost[city] + adjCost;
+                    pq.offer(new int[]{adjCity, minCost[adjCity]});
                 }
             }
         }
 
-//        System.out.println("id : " + p.id + " gain : " + p.revenue + " - " + minCost[p.dest] + " = " + (p.revenue - minCost[p.dest]));
-
-        if (minCost[p.dest] == Integer.MAX_VALUE || p.revenue - minCost[p.dest] < 0) {
-            return -1;
-        }
-
-        return p.revenue - minCost[p.dest];
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         s = 0;
-        calculated = false;
         bestQ = new PriorityQueue<>();
-        products = new Product[30001];
-        unCalculatedProductList = new ArrayList<>();
 
         Q = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
         st.nextToken();
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
+        canceled = new boolean[30001];
 
         adjList = new ArrayList[N];
         for (int i = 0; i < N; i++) {
@@ -75,6 +61,7 @@ public class Main {
             adjList[b].add(new int[]{a, w});
 
         }
+        calculate();
 //        for (int i = 0; i < N; i++) {
 //            System.out.println(i + "번 도시 인접");
 //            for (int[] adj : adjList[i]) {
@@ -82,6 +69,7 @@ public class Main {
 //            }
 //        }
 
+        query:
         for (int i = 1; i < Q; i++) {
             st = new StringTokenizer(br.readLine());
             int instruction = Integer.parseInt(st.nextToken());
@@ -90,68 +78,56 @@ public class Main {
                 int id = Integer.parseInt(st.nextToken());
                 int revenue = Integer.parseInt(st.nextToken());
                 int dest = Integer.parseInt(st.nextToken());
-                products[id] = new Product(id, revenue, dest);
-                unCalculatedProductList.add(products[id]);
+                int gain = revenue - minCost[dest];
+
+                Product p = new Product(id, revenue, dest, gain);
+//                System.out.println("상품추가 :" + p);
+                bestQ.offer(p);
+
                 continue;
             }
 
             if (instruction == 300) {
                 int id = Integer.parseInt(st.nextToken());
-                if (products[id] != null) {
-                    products[id].removed = true;
-                    products[id] = null;
-                }
+                canceled[id] = true;
                 continue;
             }
 
             if (instruction == 400) {
-                if (calculated) {
-                    for (Product p : unCalculatedProductList) {
-                        p.gain = getGain(p);
-                        if (p.gain != -1) {
-                            bestQ.offer(p);
-                        }
-                    }
-                    unCalculatedProductList = new ArrayList<>();
-
-                } else {
-                    bestQ = new PriorityQueue<>();
-                    for (int id = 1; id <= 30000; id++) {
-                        if (products[id] == null) continue;
-                        products[id].gain = getGain(products[id]);
-                        if (products[id].gain != -1) {
-                            bestQ.offer(products[id]);
-                        }
-                    }
-                    unCalculatedProductList = new ArrayList<>();
-                    calculated = true;
-                }
-
+//                System.out.println(Arrays.toString(minCost));
                 if (bestQ.isEmpty()) {
                     sb.append("-1\n");
-                } else {
-                    boolean check = false;
-                    while (!bestQ.isEmpty()) {
-                        Product p = bestQ.poll();
-                        if (p.removed) continue;
-                        int id = p.id;
-                        products[id] = null;
-                        sb.append(id).append("\n");
-                        check = true;
-                        break;
-                    }
-                    if (!check) {
-                        sb.append("-1\n");
-                    }
-
-
+                    continue;
                 }
-
+                while (!bestQ.isEmpty()) {
+                    Product p = bestQ.peek();
+                    if (p.gain < 0) {
+                        sb.append("-1\n");
+                        continue query;
+                    }
+                    p = bestQ.poll();
+                    if (!canceled[p.id]) {
+                        sb.append(p.id).append("\n");
+                        continue query;
+                    }
+                }
+                sb.append("-1\n");
                 continue;
             }
             if (instruction == 500) {
                 s = Integer.parseInt(st.nextToken());
-                calculated = false;
+                calculate();
+//                System.out.println("재계산");
+//                System.out.println(Arrays.toString(minCost));
+                List<Product> list = new ArrayList<>();
+                while (!bestQ.isEmpty()) {
+                    list.add(bestQ.poll());
+                }
+                for (Product p : list) {
+                    p.gain = p.revenue - minCost[p.dest];
+//                    System.out.println(p);
+                    bestQ.offer(p);
+                }
             }
 
         }
@@ -160,19 +136,28 @@ public class Main {
 
     static class Product implements Comparable<Product> {
         int id, revenue, dest, gain;
-        boolean removed;
 
-        public Product(int id, int revenue, int dest) {
+        public Product(int id, int revenue, int dest, int gain) {
             this.id = id;
             this.revenue = revenue;
             this.dest = dest;
-            removed = false;
+            this.gain = gain;
         }
 
         @Override
         public int compareTo(Product o) {
             if (gain == o.gain) return id - o.id;
-            return o.gain - gain;
+            return Integer.compare(o.gain, gain);
+        }
+
+        @Override
+        public String toString() {
+            return "Product{" +
+                    "id=" + id +
+                    ", revenue=" + revenue +
+                    ", dest=" + dest +
+                    ", gain=" + gain +
+                    '}';
         }
     }
 }
