34a8b03cc820af027d5caab99d4df75ae1cb72c5
HyeongJun Park <69460842+brobac@users.noreply.github.com>
2024-12-13 18:21:04 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 1608 ms, Memory: 334776 KB -BaekjoonHub

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
@@ -1,7 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -28,13 +29,7 @@ public class Main {
         }
         boolean[][][] visited = new boolean[N][M][K + 1];
 
-        PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> {
-            if (a[3] == b[3]) {
-                if (a[0] == b[0]) return b[1] - a[1];
-                return b[0] - a[0];
-            }
-            return a[3] - b[3];
-        });
+        Queue<int[]> q = new ArrayDeque<>();
         visited[0][0][0] = true;
         q.offer(new int[]{0, 0, 0, 1});
         while (!q.isEmpty()) {
@@ -44,7 +39,6 @@ public class Main {
             int k = cur[2];
             int t = cur[3];
 
-//            System.out.println("r ; " + r + " c : " + c + " k : " + k + " t : " + t);
             if (r == N - 1 && c == M - 1) {
                 System.out.println(t);
                 return;
@@ -53,25 +47,23 @@ public class Main {
             for (int d = 0; d < 4; d++) {
                 int nr = r + dr[d];
                 int nc = c + dc[d];
-                if (outOfMap(nr, nc) || visited[nr][nc][k] || map[nr][nc] == '1') continue;
-                visited[nr][nc][k] = true;
-                q.offer(new int[]{nr, nc, k, t + 1});
-            }
 
-            if (k < K) {
-                for (int d = 0; d < 4; d++) {
-                    int nr = r + dr[d];
-                    int nc = c + dc[d];
-                    if (outOfMap(nr, nc) || visited[nr][nc][k + 1] || map[nr][nc] == '0') continue;
-                    visited[nr][nc][k + 1] = true;
+                if (outOfMap(nr, nc)) continue;
+
+                if (map[nr][nc] == '0' && !visited[nr][nc][k]) {
+                    visited[nr][nc][k] = true;
+                    q.offer(new int[]{nr, nc, k, t + 1});
+                } else if (k < K && map[nr][nc] == '1' && !visited[nr][nc][k + 1]) {
                     boolean day = t % 2 == 1;
                     if (day) {
+                        visited[nr][nc][k + 1] = true;
                         q.offer(new int[]{nr, nc, k + 1, t + 1});
                     } else {
-                        q.offer(new int[]{nr, nc, k + 1, t + 2});
+                        q.offer(new int[]{r, c, k, t + 1});
                     }
                 }
             }
+
         }
         System.out.println(-1);
     }
