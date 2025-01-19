95eb207b763440cd98c411edab91f0b1176de001
HyeongJun Park <69460842+brobac@users.noreply.github.com>
2025-01-19 14:15:40 +0900

    [Gold IV] Title: 끝나지 않는 파티, Time: 300 ms, Memory: 25372 KB -BaekjoonHub

---
+++ 백준/Gold/11265. 끝나지 않는 파티/끝나지 않는 파티.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] times = new int[N][N];
+        for (int r = 0; r < N; r++) {
+            st = new StringTokenizer(br.readLine());
+            for (int c = 0; c < N; c++) {
+                times[r][c] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                if (k == i) continue;
+                for (int j = 0; j < N; j++) {
+                    times[i][j] = Math.min(times[i][j], times[i][k] + times[k][j]);
+                }
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken()) - 1;
+            int B = Integer.parseInt(st.nextToken()) - 1;
+            int C = Integer.parseInt(st.nextToken());
+
+            result.append(times[A][B] <= C ? "Enjoy other party\n" : "Stay here\n");
+
+        }
+
+        System.out.println(result);
+    }
+}
