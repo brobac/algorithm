7cd693f7f9877f828ae7c335ef04ce11a6f7b413
HyeongJun Park <69460842+brobac@users.noreply.github.com>
2025-03-08 21:08:28 +0900

    [Silver IV] Title: 상근이의 여행, Time: 152 ms, Memory: 23032 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            for (int i = 0; i < M; i++) {
+                br.readLine();
+            }
+            sb.append(N - 1).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
